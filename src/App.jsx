2e6dc4cf87aea86a526baa2f25ada62772d9275b
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-07-11 00:37:41 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./components/Hero.jsx";
+
+function App() {
+    return (
+        <div>
+            <Hero/>
+            <div>
+                <img src="../public/SectionChange.png" alt="///LOADING" />
+            </div>
+            <div className={"bg-cyberpunkYellow"}>
+                <h1>TEstt</h1>
+            </div>
+        </div>
+
+    )
+}
+
+export default App
