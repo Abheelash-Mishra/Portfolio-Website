fb49e5b74ec0b6777fe39b0cf22ba0bdc89d95e6
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-07-11 00:38:33 +0530

    Added Hero Section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,71 @@
+import {useRef, useEffect} from 'react';
+
+const Hero = () => {
+    const videoContainerRef = useRef(null);
+
+    // Find the detach button host element
+    const detachButtonHost = document.getElementById('detach-button-host');
+
+    // Check if the element exists
+    if (detachButtonHost) {
+        // Remove the detach button host element and its children
+        detachButtonHost.parentNode.removeChild(detachButtonHost);
+    }
+
+
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            const {clientX, clientY} = event;
+            const videoContainer = videoContainerRef.current;
+
+            // Calculate the movement values based on mouse position
+            const moveX = (clientX - window.innerWidth / 2) / 200;
+            const moveY = (clientY - window.innerHeight / 2) / 50;
+
+            // Apply the transformation to the video container
+            videoContainer.style.transform = `translate(${moveX}px, ${moveY}px)`;
+        };
+
+        // Add the mousemove event listener to the document
+        document.addEventListener('mousemove', handleMouseMove);
+
+        // Cleanup: Remove the event listener when the component is unmounted
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
+
+    useEffect(() => {
+        // Hide horizontal scrollbar on mount
+        document.body.style.overflowX = 'hidden';
+
+        // Cleanup: Restore default overflow on unmount
+        return () => {
+            document.body.style.overflowX = 'auto';
+        };
+    }, []);
+
+    return (
+        <div className="flex items-center justify-center min-h-screen relative overflow-hidden">
+            <div
+                ref={videoContainerRef}
+                className="absolute inset-0 z-0"
+                style={{
+                    pointerEvents: 'none',
+                    width: '100%',
+                    height: '100%',
+                }}
+            >
+                <video
+                    src="/public/Neon Sign.mkv"
+                    autoPlay
+                    muted
+                    loop
+                    className="w-full h-full object-cover scale-103"
+                ></video>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
