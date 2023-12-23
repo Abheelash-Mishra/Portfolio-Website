cbe7780500a869e044bb46c4eada5ee9e249fdb1
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-12-23 21:21:29 +0530

    Fleshed out form ui with button logics

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -7,6 +7,8 @@ const ContactMe = () => {
 	const [formData, setFormData] = useState({ "from_name": "", "from_email": "", "message": "" });
 	const [isSubmitted, setIsSubmitted] = useState(false);
 
+	const [invalidForm, setInvalidForm] = useState(false)
+
 
 	const form = useRef();
 
@@ -14,7 +16,17 @@ const ContactMe = () => {
 		e.preventDefault();
 
 		if (currInput < 2) {
-			setCurrInput(currInput + 1);
+			if (currInput === 0 && formData.from_name !== "") {
+				setCurrInput(1);
+			} else if (currInput === 1 && formData.from_email !== "") {
+				setCurrInput(2);
+			} else {
+				setInvalidForm(true);
+
+				setTimeout(() => {
+					setInvalidForm(false);
+				}, 1000)
+			}
 		}
 	};
 
@@ -29,26 +41,42 @@ const ContactMe = () => {
 	const sendEmail = (e) => {
 		e.preventDefault();
 
-		setIsSubmitted(true);
-		setCurrInput(0);
+		if (formData.message !== "") {
+
+			setIsSubmitted(true);
+			setCurrInput(0);
 
 
-		emailjs.sendForm('service_e9mw3ci', 'template_kpov14r', form.current, 'ho0x6-htbbzk46VOU')
-			.then((result) => {
-				console.log("Mail Sent!")
-			}, (error) => {
-				console.log(error);
-			});
+			// emailjs.sendForm('service_e9mw3ci', 'template_kpov14r', form.current, 'ho0x6-htbbzk46VOU')
+			// 	.then((result) => {
+			// 		console.log("Mail Sent!")
+			// 	}, (error) => {
+			// 		console.log(error);
+			// 	});
 
-		setTimeout(() => {
-			setIsSubmitted(false);
-			setFormData({ "from_name": "", "from_email": "", "message": "" });
-		}, 10000)
-
+			setTimeout(() => {
+				setIsSubmitted(false);
+				setFormData({ "from_name": "", "from_email": "", "message": "" });
+			}, 10000)
+		}
 	};
 
 	return (
-		<div className={ "w-full my-32 flex justify-center" }>
+		<div className={ "w-full flex flex-col justify-center items-center" }>
+			<div className={ "w-full" }>
+				<h1 className={ "text-xl text-aqua mb-16 mx-8 border-b-2 border-aqua/20" }>/// CONTACT.MODULE....</h1>
+			</div>
+
+			{ !isSubmitted && (
+				<div className={ "w-full flex flex-col items-center text-cyberpunkYellow px-8" }>
+					<p className={ "w-3/5 text-2xl text-justify" }>
+						Feel free to reach out and connect! I'm always open to new opportunities, collaborations, and
+						conversations. Whether you have a project in mind, want to discuss potential partnerships, or
+						simply want to say hello, I'm just a message away. Your thoughts and ideas are valuable,
+						and I'm eager to hear from you. Looking forward to connecting with you!
+					</p>
+				</div>
+			) }
 			<AnimatePresence>
 				<form ref={ form } className={ "w-2/3 px-16 py-8 text-cyberpunkYellow" }>
 					{ !isSubmitted && (
@@ -57,6 +85,7 @@ const ContactMe = () => {
 								key={ `name-${ currInput }` }
 								type="text"
 								name="from_name"
+								required
 								placeholder={ "Let's get your name first!" }
 								value={ formData.from_name }
 								onChange={ (e) => setFormData({ ...formData, "from_name": e.target.value }) }
@@ -64,13 +93,14 @@ const ContactMe = () => {
 								animate={ { opacity: 1 } }
 								exit={ { opacity: 0 } }
 								transition={ { duration: 1 } }
-								className={ `h-14 text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${ currInput !== 0 ? 'hidden' : '' }` }
+								className={ `h-14 text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${ currInput !== 0 ? 'hidden' : '' } ${invalidForm ? 'shake' : ''}` }
 							/>
 
 							<motion.input
 								key={ `email-${ currInput }` }
 								type="text"
 								name="from_email"
+								required
 								placeholder={ "Now let's get your email!" }
 								value={ formData.from_email }
 								onChange={ (e) => setFormData({ ...formData, "from_email": e.target.value }) }
@@ -78,7 +108,7 @@ const ContactMe = () => {
 								animate={ { opacity: 1 } }
 								exit={ { opacity: 0 } }
 								transition={ { duration: 1 } }
-								className={ `h-14 text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${ currInput !== 1 ? 'hidden' : '' }` }
+								className={ `h-14 text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${ currInput !== 1 ? 'hidden' : '' } ${invalidForm ? 'shake' : ''}` }
 							/>
 
 							<motion.textarea
@@ -86,14 +116,15 @@ const ContactMe = () => {
 								name="message"
 								cols={ 30 }
 								rows={ 5 }
-								placeholder={ "Drop your message here :)" }
+								required
+								placeholder={ "Drop your message here...." }
 								value={ formData.message }
 								onChange={ (e) => setFormData({ ...formData, "message": e.target.value }) }
 								initial={ { opacity: 0 } }
 								animate={ { opacity: 1 } }
 								exit={ { opacity: 0 } }
 								transition={ { duration: 1 } }
-								className={ `text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${ currInput !== 2 ? 'hidden' : '' }` }
+								className={ `text-2xl py-2 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${ currInput !== 2 ? 'hidden' : '' } ${invalidForm ? 'shake' : ''}` }
 							/>
 
 							<motion.div className={ "py-4 text-2xl" }>
@@ -117,10 +148,11 @@ const ContactMe = () => {
 								</motion.button>
 
 								<motion.button
-									whileHover={ { scale: 1.1 } }
-									whileTap={ { scale: 0.9 } }
+									whileHover={ formData.message !== "" ? { scale: 1.1 } : {} }
+									whileTap={ formData.message !== "" ? { scale: 0.9 } : {} }
 									onClick={ sendEmail }
-									className={ `border-2 border-aqua rounded-lg mx-4 px-6 py-2 hover:bg-cyberpunkYellow hover:text-black font-semibold ${ currInput !== 2 ? 'hidden' : '' }` }
+									disabled={ formData.message === "" }
+									className={ `border-2 border-aqua rounded-lg mx-4 px-6 py-2 hover:bg-cyberpunkYellow hover:text-black font-semibold ${ currInput !== 2 ? 'hidden' : '' } ${ formData.message === "" ? 'text-cyberpunkYellow/20' : 'hover:bg-cyberpunkYellow hover:text-black' }` }
 								>
 									Submit
 								</motion.button>
@@ -135,7 +167,7 @@ const ContactMe = () => {
 								animate={ { opacity: 1 } }
 								exit={ { opacity: 0 } }
 								transition={ { duration: 2 } }
-								className={ "text-3xl flex justify-center" }
+								className={ "h-[400px] text-3xl flex justify-center items-center" }
 							>
 								Thank you for your message! I'll get back to you as soon as possible :)
 							</motion.p>
