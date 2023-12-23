578a63b7c539ad17c2e072c4b099fe02833b95fb
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-12-23 10:39:15 +0530

    Added form animations

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,16 +1,13 @@
-import React, { useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 
 const ContactMe = () => {
-	const [isActive, setIsActive] = useState(false);
 	const [currInput, setCurrInput] = useState(0);
-
 	const [formData, setFormData] = useState({ "from_name": "", "from_email": "", "message": "" });
 
 
 	const form = useRef();
-	console.log(currInput)
 
 	const nextClick = (e) => {
 		e.preventDefault();
@@ -33,7 +30,6 @@ const ContactMe = () => {
 
 		emailjs.sendForm('service_e9mw3ci', 'template_kpov14r', form.current, 'ho0x6-htbbzk46VOU')
 			.then((result) => {
-				console.log("Mail sent!")
 				console.log(form.current)
 				form.current.reset();
 			}, (error) => {
@@ -42,44 +38,78 @@ const ContactMe = () => {
 
 	};
 
+
+	const inputVariants = {
+		offscreen: {
+			x: 200,
+			opacity: 0,
+		},
+		onscreen: {
+			x: 0,
+			opacity: 1,
+			transition: {
+				type: "spring",
+				bounce: 0.4,
+				duration: 2,
+				delay: 1
+			}
+		}
+	};
+
 	return (
 		<div className={ "w-full m-32 flex justify-center" }>
 			<form ref={ form } className={ "w-2/3 px-16 py-8 text-cyberpunkYellow" }>
 				<div className={ "flex flex-col py-4" }>
-					<input
+					<motion.input
+						key={`name-${currInput}`}
 						type="text"
 						name="from_name"
 						placeholder={ "Let's get your name first!" }
 						value={ formData.from_name }
 						onChange={ (e) => setFormData({ ...formData, "from_name": e.target.value }) }
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 1 }}
 						className={ `h-14 text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${currInput !== 0 ? 'hidden' : ''}` }
 					/>
 
-					<input
+					<motion.input
+						key={`email-${currInput}`}
 						type="text"
 						name="from_email"
 						placeholder={ "Now let's get your email!" }
 						value={ formData.from_email }
 						onChange={ (e) => setFormData({ ...formData, "from_email": e.target.value }) }
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 1 }}
 						className={ `h-14 text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${currInput !== 1 ? 'hidden' : ''}` }
 					/>
 
-					<textarea
+					<motion.textarea
+						key={`message-${currInput}`}
 						name="message"
 						cols={ 30 }
 						rows={ 5 }
 						placeholder={ "Drop your message here :)" }
 						value={ formData.message }
 						onChange={ (e) => setFormData({ ...formData, "message": e.target.value }) }
+						initial={{ opacity: 0 }}
+						animate={{ opacity: 1 }}
+						exit={{ opacity: 0 }}
+						transition={{ duration: 1 }}
 						className={ `text-2xl py-1 px-4 font-semibold bg-black border-2 rounded-lg border-aqua focus:outline-none placeholder-aqua/40 ${currInput !== 2 ? 'hidden' : ''}` }
 					/>
 
-					<div className={ "py-4 text-2xl" }>
+					<motion.div className={ "py-4 text-2xl" }>
 						<motion.button
-							whileHover={ { scale: 1.1 } }
-							whileTap={ { scale: 0.9 } }
+							whileHover={ currInput !== 0 ? { scale: 1.1 } : {} }
+							whileTap={ currInput !== 0 ? { scale: 0.9 } : {} }
 							onClick={ backClick }
-							className={ `border-2 border-aqua rounded-lg px-6 py-2 hover:bg-cyberpunkYellow hover:text-black font-semibold ${currInput === 0 ? 'disabled:bg-blue' : ''}` }
+							disabled={ currInput === 0 }
+							className={ `border-2 border-aqua rounded-lg px-6 py-2 font-semibold ${ currInput === 0 ? 'text-cyberpunkYellow/20' : 'hover:bg-cyberpunkYellow hover:text-black' }` }
 						>
 							Back
 						</motion.button>
@@ -88,7 +118,7 @@ const ContactMe = () => {
 							whileHover={ { scale: 1.1 } }
 							whileTap={ { scale: 0.9 } }
 							onClick={ nextClick }
-							className={ `border-2 border-aqua rounded-lg mx-4 px-6 py-2 hover:bg-cyberpunkYellow hover:text-black font-semibold ${currInput !== 2 ? '' : 'hidden'}` }
+							className={ `border-2 border-aqua rounded-lg mx-4 px-6 py-2 hover:bg-cyberpunkYellow hover:text-black font-semibold ${ currInput !== 2 ? '' : 'hidden' }` }
 						>
 							Next
 						</motion.button>
@@ -101,7 +131,7 @@ const ContactMe = () => {
 						>
 							Submit
 						</motion.button>
-					</div>
+					</motion.div>
 				</div>
 			</form>
 
