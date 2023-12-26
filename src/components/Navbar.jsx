ecfa19be99d4ea39301386b72e434950784d65fe
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-12-26 19:44:35 +0530

    Changed navbar to switch to black bg after scrolling down

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,49 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
-	const [isYellowBg, setIsYellowBg] = useState(false);
+	const [navBackground, setNavBackground] = useState('transparent');
+
+	const handleScroll = () => {
+		const scrollPosition = window.scrollY;
+
+		const changePosition = 800;
+
+		if (scrollPosition > changePosition) {
+			setNavBackground('black');
+		} else {
+			setNavBackground('transparent');
+		}
+	};
 
 	useEffect(() => {
-		const observer = new IntersectionObserver((entries) => {
-			setIsYellowBg(entries[0].isIntersecting);
-		}, {threshold: 0.5});
-		observer.observe(document.getElementById("yellow-bg"));
+		document.addEventListener('scroll', handleScroll);
+		return () => {
+			document.removeEventListener('scroll', handleScroll);
+		};
 	}, []);
 
 	return (
 		<nav
-			className={ `flex flex-row justify-between text-${
-				isYellowBg ? "black" : "cyberpunkYellow"
-			} items-center text-2xl pt-4 px-8 bg-transparent fixed top-0 left-0 w-full z-50` }
+			className={ `flex flex-row justify-between items-center text-2xl pt-4 px-8 bg-${navBackground} fixed top-0 left-0 w-full z-50 border-b-2 border-aqua` }
 		>
 			<div className={"flex flex-row"}>
 				<a href="#hero">
-					<img src={`${isYellowBg ? "/black_sign.png" : "/yellow_sign.png"}`} alt="Abheelash" className={"w-1/4 transform -rotate-3"} />
+					<img src={"/yellow_sign.png"} alt="Abheelash" className={"w-1/4 transform -rotate-3"} />
 				</a>
 			</div>
-			<div className={"flex flex-row"}>
-				<a href={"#aboutMe"} className={ `text-${isYellowBg ? "black" : "cyberpunkYellow"} items-center text-2xl px-4 font-semibold` }>// About Me</a>
-				<a className={ `text-${isYellowBg ? "black" : "cyberpunkYellow"} items-center text-2xl px-4 font-semibold` }>// Navbar 2</a>
-				<a className={ `text-${isYellowBg ? "black" : "cyberpunkYellow"} items-center text-2xl px-4 font-semibold` }>// Navbar 3</a>
+			<div className={ "flex flex-row" }>
+				<a href={ "#aboutMe" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+					// About Me
+				</a>
+				<a href={ "" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+					// Projects
+				</a>
+				<a href={ "" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+					// Experience
+				</a>
+				<a href={ "" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+					// Contact Me
+				</a>
 			</div>
 		</nav>
 	);
