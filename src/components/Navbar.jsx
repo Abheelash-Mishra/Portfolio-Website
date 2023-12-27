258cc4eba381ae1eda8476c43727b94c6d37cf37
Abheelash-Mishra <abheelashmishra@gmail.com>
2023-12-27 12:31:09 +0530

    Navbar partially responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,24 +24,24 @@ const Navbar = () => {
 
 	return (
 		<nav
-			className={ `flex flex-row justify-between items-center text-2xl pt-4 px-8 bg-${navBackground} fixed top-0 left-0 w-full z-50 border-b-2 border-aqua` }
+			className={ `flex flex-row justify-between items-center text-2xl pt-4 lg:px-8 sm:px-1 bg-${navBackground} fixed top-0 left-0 w-full z-50 border-b-2 border-aqua` }
 		>
 			<div className={"flex flex-row"}>
-				<a href="#hero">
-					<img src={"/yellow_sign.png"} alt="Abheelash" className={"w-1/4 transform -rotate-3"} />
+				<a href="#hero" className={"lg:max-w-[250px] sm:max-w-[150px]"}>
+					<img src={"/yellow_sign.png"} alt="Abheelash" className={"max-w-full transform -rotate-3"} />
 				</a>
 			</div>
-			<div className={ "flex flex-row" }>
-				<a href={ "#aboutMe" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+			<div className={ "flex flex-row lg:text-2xl sm:text-lg" }>
+				<a href={ "#aboutMe" } className={ `text-cyberpunkYellow items-center px-4 font-semibold` }>
 					// About Me
 				</a>
-				<a href={ "" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+				<a href={ "" } className={ `text-cyberpunkYellow items-center px-4 font-semibold` }>
 					// Projects
 				</a>
-				<a href={ "" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+				<a href={ "" } className={ `text-cyberpunkYellow items-center px-4 font-semibold` }>
 					// Experience
 				</a>
-				<a href={ "" } className={ `text-cyberpunkYellow items-center text-2xl px-4 font-semibold` }>
+				<a href={ "" } className={ `text-cyberpunkYellow items-center px-4 font-semibold` }>
 					// Contact Me
 				</a>
 			</div>
